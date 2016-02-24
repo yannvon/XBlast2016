1808dffd88fbc4b1cac2c61914a9d9902d3985cd
Yann Vonlanthen <yann.vonlanthen@epfl.ch>
2016-02-24 14:26:41 +0100

    adding first version of SubCell

---
+++ src/ch/epfl/xblast/SubCell.java
@@ -0,0 +1,18 @@
+package ch.epfl.xblast;
+
+/**
+ * Represents a sub cell.
+ * 
+ * @author Loïc Vandenberghe (257742)
+ * @author Yann (258857)
+ *
+ */
+public final class SubCell {
+    
+
+   
+    public SubCell(int x, int y) {
+        
+    }
+
+}
