9dbfe83f88f1ba420ca6fe5674091a44df4f5be2
Yann Vonlanthen <yann.vonlanthen@epfl.ch>
2016-02-26 15:41:42 +0100

    first version of class Lists

---
+++ src/ch/epfl/xblast/Lists.java
@@ -0,0 +1,23 @@
+package ch.epfl.xblast;
+
+import java.util.List;
+
+/**
+ * Non-instantiable class containing static methods to work on Lists.
+ * 
+ * @author Loïc Vandenberghe (257742)
+ * @author Yann Vonlanthen (258857)
+ *
+ */
+public final class Lists {
+
+    
+    private Lists() {
+    }
+    
+    public static <T> List<T> mirrored(List<T> l){
+        if(l.isEmpty()){
+            
+        }
+    }
+}
