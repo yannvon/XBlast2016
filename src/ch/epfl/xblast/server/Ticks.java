6572f40e5389253112b0e0ae6ef4195de5af258a
LoicVandenberghe <loic.vandenberghe@epfl.ch>
2016-02-26 14:33:00 +0100

    Ajout de l'interface Ticks

---
+++ src/ch/epfl/xblast/server/Ticks.java
@@ -0,0 +1,14 @@
+package ch.epfl.xblast.server;
+
+public interface Ticks {
+    
+    //constants
+    public static final int PLAYER_DYING_TICKS = 8;
+    public static final int PLAYER_INVULNERABLE_TICKS = 64;
+    public static final int BOMB_FUSE_TICKS = 100;
+    public static final int EXPLOSION_TICKS = 30;
+    public static final int WALL_CRUMBLING_TICKS = 30;
+    public static final int BONUS_DISAPPEARING_TICKS = 30;
+    
+    
+}
