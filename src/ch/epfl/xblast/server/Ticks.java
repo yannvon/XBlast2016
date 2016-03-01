81c438973f3469e3f7691763fd4a94c5940b6660
Yann Vonlanthen <yann.vonlanthen@epfl.ch>
2016-03-01 21:41:53 +0100

    Adding comments to ticks.java

--- src/ch/epfl/xblast/server/Ticks.java
+++ src/ch/epfl/xblast/server/Ticks.java
@@ -1,7 +1,14 @@
 package ch.epfl.xblast.server;
 
+/**
+ * Interface serving the purpose of defining multiple aspects related to time
+ * management inside the game.
+ * 
+ * @author Loic Vandenberghe (257742)
+ * @author Yann Vonlanthen (258857)
+ */
 public interface Ticks {
-    
+
     // Constants
     public static final int PLAYER_DYING_TICKS = 8;
     public static final int PLAYER_INVULNERABLE_TICKS = 64;
@@ -9,5 +16,5 @@ public interface Ticks {
     public static final int EXPLOSION_TICKS = 30;
     public static final int WALL_CRUMBLING_TICKS = 30;
     public static final int BONUS_DISAPPEARING_TICKS = 30;
-    
+
 }
