b12e21046b622834fcd79d105af8ba0da294d6e0
Yann Vonlanthen <yann.vonlanthen@epfl.ch>
2016-03-01 22:36:21 +0100

    tiny changes to Board

--- src/ch/epfl/xblast/server/Board.java
+++ src/ch/epfl/xblast/server/Board.java
@@ -4,43 +4,60 @@ import java.util.ArrayList;
 import java.util.List;
 
 import ch.epfl.cs108.Sq;
+import ch.epfl.xblast.Cell;
 
 /**
+ * The immutable class Board represents the Game Board of the xblast game.
+ * 
  * @author Loic Vandenberghe (257742)
  * @author Yann Vonlanthen (258857)
  *
  */
 public final class Board {
 
-   private List<Sq<Block>> board;
-       
+    // Attributes
+    private List<Sq<Block>> board; // FIXME final since immutable class?
+
     /**
-     * constructor
+     * Constructor of a board taking a list of block sequences as param.
+     * 
      * @param blocks
-     * @throws IllegalArgumentException if the size of the List is not correct
+     *            a list of block sequences
+     * @throws IllegalArgumentException
+     *             if the size of the List is not correct
      */
-    public Board(List<Sq<Block>> blocks){
-        if(board.size() != 195){
-            throw new IllegalArgumentException();
+    public Board(List<Sq<Block>> blocks) {
+        if (board.size() != Cell.COUNT) {// FIXME added constant instead of 195
+            throw new IllegalArgumentException();   //FIXME add message?
         }
-        
-        board=new ArrayList<>(blocks);
-       
+
+        board = new ArrayList<>(blocks); // FIXME cant use List?
+
     }
-    
-    public static final Board ofRows(List<List<Block>> rows){
-        //TODO
-        for(int i=0; i<rows.size(); ){
-            
+
+    /**
+     * Constructs a constant Board, a given block matrix.
+     * 
+     * @param rows
+     *            a matrix of blocks
+     * @return a constant Board
+     * @throws IllegalArgumentException
+     *             if the list rows is not in the right format
+     */
+    public static final Board ofRows(List<List<Block>> rows) {
+        // TODO
+        for (int i = 0; i < rows.size();) {
+
         }
-        
+
         return null;
     }
     
-    public static final void checkBlockMatrix(List<List<Block>> matrix, 
+    
+
+    public static final void checkBlockMatrix(List<List<Block>> matrix,
             int rows, int columns) {
-        //TODO
-        
-        
+        // TODO
+
     }
 }
