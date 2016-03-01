39dc38daa3ba17d8f65c162244a511ecf86e4015
Yann Vonlanthen <yann.vonlanthen@epfl.ch>
2016-03-01 21:34:46 +0100

    Adding comments to block.java

--- src/ch/epfl/xblast/server/Block.java
+++ src/ch/epfl/xblast/server/Block.java
@@ -1,23 +1,40 @@
 package ch.epfl.xblast.server;
 
+/**
+ * Enum of Blocks.
+ * 
+ * @author Loic Vandenberghe (257742)
+ * @author Yann Vonlanthen (258857)
+ */
 public enum Block {
-    
-    FREE,
-    INDESTRUCTIBLE_WALL,
-    DESTRUCTIBLE_WALL,
-    CRUMBLING_WALL;
-    
-    public boolean isFree(){
-        return this==FREE;
+
+    FREE, INDESTRUCTIBLE_WALL, DESTRUCTIBLE_WALL, CRUMBLING_WALL;
+
+    /**
+     * Determines if the Block is free.
+     * 
+     * @return true is the block is free, false otherwise
+     */
+    public boolean isFree() {
+        return this == FREE;
     }
-    
-    public boolean canHostPlayer(){
-        //TODO
-        return this==FREE;
+
+    /**
+     * Defines if the Block can host a player.
+     * 
+     * @return true if the block can host a player, false otherwise
+     */
+    public boolean canHostPlayer() {
+        // TODO
+        return this == FREE;
     }
-    
-    public boolean castsShadow(){
-        switch(this){
+
+    /**
+     * Defines if the Blocks casts a shadow.
+     * @return true if the Block casts a shadow, false otherwise
+     */
+    public boolean castsShadow() {
+        switch (this) {
         case INDESTRUCTIBLE_WALL:
         case DESTRUCTIBLE_WALL:
         case CRUMBLING_WALL:
@@ -26,5 +43,5 @@ public enum Block {
             return false;
         }
     }
-    
+
 }
