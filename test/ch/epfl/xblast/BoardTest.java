e2f3e7703d2da788f59b503b4ce744ccdda2ee43
LoicVandenberghe <loic.vandenberghe@epfl.ch>
2016-03-02 20:06:13 +0100

    adding 3 test WARNING: InnerBlocks seems not working

--- test/ch/epfl/xblast/BoardTest.java
+++ test/ch/epfl/xblast/BoardTest.java
@@ -13,6 +13,10 @@ import ch.epfl.xblast.server.Block;
 import ch.epfl.xblast.server.Board;
 
 public class BoardTest {
+    Block __ = Block.FREE;
+    Block XX = Block.INDESTRUCTIBLE_WALL;
+    Block xx = Block.DESTRUCTIBLE_WALL;
+ 
 
     @Test(expected = java.lang.IllegalArgumentException.class)
     public void constructorSizeException() {
@@ -34,51 +38,10 @@ public class BoardTest {
         Board a=Board.ofInnerBlocksWalled(list);
     }
     
+  
+ 
     @Test
-    public void constructor(){
-        Block __ = Block.FREE;
-        Block XX = Block.INDESTRUCTIBLE_WALL;
-        Block xx = Block.DESTRUCTIBLE_WALL;
-       
-        
-        
-        
-        Board boardOfQuadrantNW = Board.ofQuadrantNWBlocksWalled(
-                Arrays.asList(
-                        Arrays.asList(__, __, __, __, __, xx, __),
-                        Arrays.asList(__, XX, xx, XX, xx, XX, xx),
-                        Arrays.asList(__, xx, __, __, __, xx, __),
-                        Arrays.asList(xx, XX, __, XX, XX, XX, XX),
-                        Arrays.asList(__, xx, __, xx, __, __, __),
-                        Arrays.asList(xx, XX, xx, XX, xx, XX, __)));
-        
-        Board boardOfInnerBlocksWalled= Board.ofInnerBlocksWalled( Arrays.asList(
-                Arrays.asList( __, __, __, __, __, xx, __, xx, __, __, __,
-                        __, __),
-                Arrays.asList( __, __, __, __, __, xx, __, xx, __, __, __,
-                        __, __),
-                Arrays.asList( __, __, __, __, __, xx, __, xx, __, __, __,
-                        __, __),
-                Arrays.asList( __, __, __, __, __, xx, __, xx, __, __, __,
-                        __, __),
-                Arrays.asList( __, __, __, __, __, xx, __, xx, __, __, __,
-                        __, __),
-                Arrays.asList( __, __, __, __, __, xx, __, xx, __, __, __,
-                        __, __),
-                Arrays.asList( __, __, __, __, __, xx, __, xx, __, __, __,
-                        __, __),
-                Arrays.asList( __, __, __, __, __, xx, __, xx, __, __, __,
-                        __, __),
-                Arrays.asList( __, __, __, __, __, xx, __, xx, __, __, __,
-                        __, __),
-                Arrays.asList( __, __, __, __, __, xx, __, xx, __, __, __,
-                        __, __),
-                Arrays.asList( __, __, __, __, __, xx, __, xx, __, __, __,
-                        __, __)
-                
-                ));
-        
-
+    public void show(){
         Board boardOfRows = Board.ofRows(Arrays.asList(
                 Arrays.asList(XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX,
                         XX, XX, XX),
@@ -106,8 +69,182 @@ public class BoardTest {
                         __, __, XX),
                 Arrays.asList(XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX,
                         XX, XX, XX)));
+        
+    Board boardOfQuadrant = Board.ofQuadrantNWBlocksWalled(
+            Arrays.asList(
+                    Arrays.asList(__, __, __, __, __, xx, __),
+                    Arrays.asList(__, XX, xx, XX, xx, XX, xx),
+                    Arrays.asList(__, xx, __, __, __, xx, __),
+                    Arrays.asList(xx, XX, __, XX, XX, XX, XX),
+                    Arrays.asList(__, xx, __, xx, __, __, __),
+                    Arrays.asList(xx, XX, xx, XX, xx, XX, __)));
+    
+    Board boardOfInner= Board.ofInnerBlocksWalled( Arrays.asList(
+            Arrays.asList( __, __, __, __, __, xx, __, xx, __, __, __,
+                    __, __),
+            Arrays.asList( __, XX, xx, XX, xx, XX, xx, XX, xx, XX, xx,
+                    XX, __),
+            Arrays.asList( __, xx, __, __, __, xx, __, xx, __, __, __,
+                    xx, __),
+            Arrays.asList( xx, XX, __, XX, XX, XX, XX, XX, XX, XX, __,
+                    XX, xx),
+            Arrays.asList( __, xx, __, xx, __, __, __, __, __, xx, __,
+                    xx, __),
+            Arrays.asList( xx, XX, xx, XX, xx, XX, __, XX, xx, XX, xx,
+                    XX, xx),
+            Arrays.asList( __, xx, __, xx, __, __, __, __, __, xx, __,
+                    xx, __),
+            Arrays.asList( xx, XX, __, XX, XX, XX, XX, XX, XX, XX, __,
+                    XX, xx),
+            Arrays.asList( __, xx, __, __, __, xx, __, xx, __, __, __,
+                    xx, __),
+            Arrays.asList( __, XX, xx, XX, xx, XX, xx, XX, xx, XX, xx,
+                    XX, __),
+            Arrays.asList( __, __, __, __, __, xx, __, xx, __, __, __,
+                    __, __)
+            ));
+    
+        System.out.println("\n\tBoard of rows:");
+        
+        for (int i = 0; i < Cell.ROWS; i++) {
+            for (int j = 0; j < Cell.COLUMNS; j++) {
+                System.out
+                        .print(boardOfRows.blockAt(new Cell(j, i)) + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n\tBoard of inner blocks walled:");
+        
+        
+        for (int i = 0; i < Cell.ROWS; i++) {
+            for (int j = 0; j < Cell.COLUMNS; j++) {
+                System.out
+                        .print(boardOfInner.blockAt(new Cell(j, i)) + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n\tBoard of quadrant NW blocks walled:");
+        
+        
+        for (int i = 0; i < Cell.ROWS; i++) {
+            for (int j = 0; j < Cell.COLUMNS; j++) {
+                System.out
+                        .print(boardOfQuadrant.blockAt(new Cell(j, i)) + " ");
+            }
+            System.out.println();
+        }
+        
+        
     }
     
     
+    @Test
+    public void ofRowsVsOfQuadrant(){
+        Board boardOfRows = Board.ofRows(Arrays.asList(
+                Arrays.asList(XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX,
+                        XX, XX, XX),
+                Arrays.asList(XX, __, __, __, __, __, xx, __, xx, __, __, __,
+                        __, __, XX),
+                Arrays.asList(XX, __, XX, xx, XX, xx, XX, xx, XX, xx, XX, xx,
+                        XX, __, XX),
+                Arrays.asList(XX, __, xx, __, __, __, xx, __, xx, __, __, __,
+                        xx, __, XX),
+                Arrays.asList(XX, xx, XX, __, XX, XX, XX, XX, XX, XX, XX, __,
+                        XX, xx, XX),
+                Arrays.asList(XX, __, xx, __, xx, __, __, __, __, __, xx, __,
+                        xx, __, XX),
+                Arrays.asList(XX, xx, XX, xx, XX, xx, XX, __, XX, xx, XX, xx,
+                        XX, xx, XX),
+                Arrays.asList(XX, __, xx, __, xx, __, __, __, __, __, xx, __,
+                        xx, __, XX),
+                Arrays.asList(XX, xx, XX, __, XX, XX, XX, XX, XX, XX, XX, __,
+                        XX, xx, XX),
+                Arrays.asList(XX, __, xx, __, __, __, xx, __, xx, __, __, __,
+                        xx, __, XX),
+                Arrays.asList(XX, __, XX, xx, XX, xx, XX, xx, XX, xx, XX, xx,
+                        XX, __, XX),
+                Arrays.asList(XX, __, __, __, __, __, xx, __, xx, __, __, __,
+                        __, __, XX),
+                Arrays.asList(XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX,
+                        XX, XX, XX)));
+        
+        Board boardOfQuadrant = Board.ofQuadrantNWBlocksWalled(
+            Arrays.asList(
+                    Arrays.asList(__, __, __, __, __, xx, __),
+                    Arrays.asList(__, XX, xx, XX, xx, XX, xx),
+                    Arrays.asList(__, xx, __, __, __, xx, __),
+                    Arrays.asList(xx, XX, __, XX, XX, XX, XX),
+                    Arrays.asList(__, xx, __, xx, __, __, __),
+                    Arrays.asList(xx, XX, xx, XX, xx, XX, __)));
+    
+        for (int i = 0; i < Cell.ROWS; i++) {
+            for (int j = 0; j < Cell.COLUMNS; j++) {
+                assertEquals(boardOfQuadrant.blockAt(new Cell(j, i)),
+                    boardOfRows.blockAt(new Cell(j, i)));
+            }
+        }
 
+    }
+    public void ofRowsVsOfInner(){
+        Board boardOfRows = Board.ofRows(Arrays.asList(
+                Arrays.asList(XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX,
+                        XX, XX, XX),
+                Arrays.asList(XX, __, __, __, __, __, xx, __, xx, __, __, __,
+                        __, __, XX),
+                Arrays.asList(XX, __, XX, xx, XX, xx, XX, xx, XX, xx, XX, xx,
+                        XX, __, XX),
+                Arrays.asList(XX, __, xx, __, __, __, xx, __, xx, __, __, __,
+                        xx, __, XX),
+                Arrays.asList(XX, xx, XX, __, XX, XX, XX, XX, XX, XX, XX, __,
+                        XX, xx, XX),
+                Arrays.asList(XX, __, xx, __, xx, __, __, __, __, __, xx, __,
+                        xx, __, XX),
+                Arrays.asList(XX, xx, XX, xx, XX, xx, XX, __, XX, xx, XX, xx,
+                        XX, xx, XX),
+                Arrays.asList(XX, __, xx, __, xx, __, __, __, __, __, xx, __,
+                        xx, __, XX),
+                Arrays.asList(XX, xx, XX, __, XX, XX, XX, XX, XX, XX, XX, __,
+                        XX, xx, XX),
+                Arrays.asList(XX, __, xx, __, __, __, xx, __, xx, __, __, __,
+                        xx, __, XX),
+                Arrays.asList(XX, __, XX, xx, XX, xx, XX, xx, XX, xx, XX, xx,
+                        XX, __, XX),
+                Arrays.asList(XX, __, __, __, __, __, xx, __, xx, __, __, __,
+                        __, __, XX),
+                Arrays.asList(XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX,
+                        XX, XX, XX)));
+    
+        Board boardOfInner= Board.ofInnerBlocksWalled( Arrays.asList(
+                Arrays.asList( __, __, __, __, __, xx, __, xx, __, __, __,
+                        __, __),
+                Arrays.asList( __, XX, xx, XX, xx, XX, xx, XX, xx, XX, xx,
+                        XX, __),
+                Arrays.asList( __, xx, __, __, __, xx, __, xx, __, __, __,
+                        xx, __),
+                Arrays.asList( xx, XX, __, XX, XX, XX, XX, XX, XX, XX, __,
+                        XX, xx),
+                Arrays.asList( __, xx, __, xx, __, __, __, __, __, xx, __,
+                        xx, __),
+                Arrays.asList( xx, XX, xx, XX, xx, XX, __, XX, xx, XX, xx,
+                        XX, xx),
+                Arrays.asList( __, xx, __, xx, __, __, __, __, __, xx, __,
+                        xx, __),
+                Arrays.asList( xx, XX, __, XX, XX, XX, XX, XX, XX, XX, __,
+                        XX, xx),
+                Arrays.asList( __, xx, __, __, __, xx, __, xx, __, __, __,
+                        xx, __),
+                Arrays.asList( __, XX, xx, XX, xx, XX, xx, XX, xx, XX, xx,
+                        XX, __),
+                Arrays.asList( __, __, __, __, __, xx, __, xx, __, __, __,
+                        __, __)
+                ));
+        for (int i = 0; i < Cell.ROWS; i++) {
+            for (int j = 0; j < Cell.COLUMNS; j++) {
+                assertEquals(boardOfInner.blockAt(new Cell(j, i)),
+                    boardOfRows.blockAt(new Cell(j, i)));
+            }
+        }
+    }
 }
