51b8ff4ed9cf9999a022269f128268cb00e05798
Yann Vonlanthen <yann.vonlanthen@epfl.ch>
2016-03-04 14:27:03 +0100

    Adding comments to ArgumentChecker

--- src/ch/epfl/xblast/ArgumentChecker.java
+++ src/ch/epfl/xblast/ArgumentChecker.java
@@ -1,7 +1,8 @@
 package ch.epfl.xblast;
 
 /**
- * Non-instantiable class that contains methods to check an argument
+ * Non-instantiable class that contains methods to check validity of an
+ * argument.
  * 
  * @author Loic Vandenberghe (257742)
  * @author Yann Vonlanthen (258857)
@@ -10,15 +11,24 @@ package ch.epfl.xblast;
 public final class ArgumentChecker {
 
     /**
-     * private constructor
+     * Constructor: private since class should not be instantiable.
      */
     private ArgumentChecker() {
 
     }
 
+    /**
+     * Determines if given integer is non-negative
+     * 
+     * @param value
+     *            integer that has to be checked
+     * @return same value, if argument was non-negative
+     * @throws IllegalArgumentException
+     *             if the argument was negative
+     */
     public static int requireNonNegative(int value) {
         if (value < 0) {
-            throw new IllegalArgumentException("negative value");
+            throw new IllegalArgumentException("Negative value!");
         }
         return value;
     }
