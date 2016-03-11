372df8f3e77cd3bd2fcfb5e67db8ef78b55f65b4
Yann Vonlanthen <yann.vonlanthen@epfl.ch>
2016-03-11 12:14:07 +0100

    Changing Block according to Week 4

--- src/ch/epfl/xblast/server/Block.java
+++ src/ch/epfl/xblast/server/Block.java
@@ -1,5 +1,7 @@
 package ch.epfl.xblast.server;
 
+import java.util.NoSuchElementException;
+
 /**
  * Enum of Blocks.
  * 
@@ -8,7 +10,32 @@ package ch.epfl.xblast.server;
  */
 public enum Block {
 
-    FREE, INDESTRUCTIBLE_WALL, DESTRUCTIBLE_WALL, CRUMBLING_WALL;
+    FREE, INDESTRUCTIBLE_WALL, DESTRUCTIBLE_WALL, CRUMBLING_WALL, BONUS_BOMB(
+            Bonus.INC_BOMB), BONUS_RANGE(Bonus.INC_RANGE);
+
+    // Attributes
+    // contains either corresponding bonus or null if there is none.
+    private Bonus maybeAssociatedBonus;
+
+    /**
+     * Primary constructor, used by the Bonus Blocks.
+     * 
+     * @param maybeAssociatedBonus
+     *            Bonus associated to block
+     */
+    private Block(Bonus maybeAssociatedBonus) { // FIXME test nonulle? lancer ereure?
+        // storing the associated bonus
+        this.maybeAssociatedBonus = maybeAssociatedBonus;
+    }
+
+    
+    /**
+     * Default constructor, used by all other blocks.
+     */
+    private Block() {
+        // this block has no associated bonus
+        this.maybeAssociatedBonus = null;
+    }
 
     /**
      * Determines if the Block is free.
@@ -25,16 +52,16 @@ public enum Block {
      * @return true if the block can host a player, false otherwise
      */
     public boolean canHostPlayer() {
-        // TODO
-        return this == FREE;
+        return (this == FREE || isBonus());
     }
 
     /**
      * Defines if the Blocks casts a shadow.
+     * 
      * @return true if the Block casts a shadow, false otherwise
      */
     public boolean castsShadow() {
-        switch (this) {
+        switch (this) { //FIXME logic expression instead?
         case INDESTRUCTIBLE_WALL:
         case DESTRUCTIBLE_WALL:
         case CRUMBLING_WALL:
@@ -42,5 +69,35 @@ public enum Block {
         default:
             return false;
         }
+    }
+
+    /**
+     * Checks if Block is a bonus or not.
+     * 
+     * @return true if it is a bonus, false otherwise
+     */
+    public boolean isBonus() {  //FIXME use logic expression instead of switch?
+        switch (this) {
+        case BONUS_BOMB:
+        case BONUS_RANGE:
+            return true;
+        default:
+            return false;
+        }
+    }
+
+    /**
+     * Returns the bonus associated with this Block or throws an exception if
+     * there is no bonus associated.
+     * 
+     * @return the bonus associated to the block
+     * @throws NoSuchElementException
+     *             if the block has no associated bonus
+     */
+    public Bonus associatedBonus() {
+        if (maybeAssociatedBonus == null) {
+            throw new NoSuchElementException("This Block does not contain a bonus");
+        }
+        return maybeAssociatedBonus;
     }
 }
