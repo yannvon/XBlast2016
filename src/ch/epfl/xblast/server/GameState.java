b457fa173185826208804f9adc4e73e6998fe87c
Yann Vonlanthen <yann.vonlanthen@epfl.ch>
2016-03-11 15:53:57 +0100

    first compilable version of Week4. TODO: tests.

--- src/ch/epfl/xblast/server/GameState.java
+++ src/ch/epfl/xblast/server/GameState.java
@@ -147,4 +147,37 @@ public final class GameState {
         }
         return alivePlayers;
     }
+    //TODO other methods (next())
+    
+    
+    /*
+     * private methods
+     */
+
+    /**
+     * @param blasts0
+     * @param board0
+     * @param explosions0
+     * @return
+     */
+    private List<Sq<Cell>> nextBlasts(List<Sq<Cell>> blasts0, Board board0,
+            List<Sq<Sq<Cell>>> explosions0) {
+        List<Sq<Cell>> blasts1 = new ArrayList<>(); //FIXME right choice?
+        
+        // add existing blasts
+        for(Sq<Cell> blast : blasts0){
+            Sq<Cell> newBlast=blast.tail();
+
+            if (board0.blockAt(blast.head()).isFree() && !newBlast.isEmpty()) {
+                blasts1.add(newBlast);
+            }
+        }
+        
+        // add new blasts
+        for(Sq<Sq<Cell>> arm : explosions0){
+            blasts1.add(arm.head());
+        }
+        
+        return blasts1;
+    }
 }
