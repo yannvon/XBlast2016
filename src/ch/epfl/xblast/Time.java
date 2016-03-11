64a467f756fa857f5687cfe3f2d27719b7c259b5
Yann Vonlanthen <yann.vonlanthen@epfl.ch>
2016-03-11 12:52:50 +0100

    Adding Time interface and updating Ticks

---
+++ src/ch/epfl/xblast/Time.java
@@ -0,0 +1,28 @@
+package ch.epfl.xblast;
+
+/**
+ * Time interface defining multiple, time related constants.
+ * 
+ * @author Loïc Vandenberghe (257742)
+ * @author Yann Vonlanthen (258857)
+ *
+ */
+public interface Time {
+    /**
+     * seconds per minute   FIXME ask assistant if comments like this are good or not.
+     */
+    public static final int S_PER_MIN = 60;
+    /**
+     * milliseconds per second
+     */
+    public static final int MS_PER_S = 1000;
+    /**
+     * microseconds per second
+     */
+    public static final int US_PER_S = 1000 * MS_PER_S;
+    /**
+     * nanoseconds per second
+     */
+    public static final int NS_PER_S = 1000 * US_PER_S;
+
+}
--- src/ch/epfl/xblast/server/Ticks.java
+++ src/ch/epfl/xblast/server/Ticks.java
@@ -1,5 +1,7 @@
 package ch.epfl.xblast.server;
 
+import ch.epfl.xblast.Time;
+
 /**
  * Interface serving the purpose of defining multiple aspects related to time
  * management inside the game.
@@ -10,11 +12,15 @@ package ch.epfl.xblast.server;
 public interface Ticks {
 
     // Constants
-    public static final int PLAYER_DYING_TICKS = 8;
+    public static final int PLAYER_DYING_TICKS = 8;       //FIXME commenting like TIME?
     public static final int PLAYER_INVULNERABLE_TICKS = 64;
     public static final int BOMB_FUSE_TICKS = 100;
     public static final int EXPLOSION_TICKS = 30;
     public static final int WALL_CRUMBLING_TICKS = 30;
     public static final int BONUS_DISAPPEARING_TICKS = 30;
+    
+    public static final int TICKS_PER_SECOND = 20;
+    public static final int TICK_NANOSECOND_DURATION = Time.NS_PER_S / TICKS_PER_SECOND;
+    public static final int TOTAL_TICKS = TICKS_PER_SECOND* Time.S_PER_MIN * 2; //FIXME
 
 }
