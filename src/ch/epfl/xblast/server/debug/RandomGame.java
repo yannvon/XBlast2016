337522cdb3bc5b73ca73b102712ccccf737c3448
Yann Vonlanthen <yann.vonlanthen@epfl.ch>
2016-03-31 17:33:07 +0200

    adding RandomGame class

---
+++ src/ch/epfl/xblast/server/debug/RandomGame.java
@@ -0,0 +1,65 @@
+package ch.epfl.xblast.server.debug;
+
+import java.util.Arrays;
+import java.util.List;
+
+import ch.epfl.xblast.Cell;
+import ch.epfl.xblast.PlayerID;
+import ch.epfl.xblast.server.Block;
+import ch.epfl.xblast.server.Board;
+import ch.epfl.xblast.server.GameState;
+import ch.epfl.xblast.server.Player;
+
+/**
+ * Executable class that displays a random game.
+ * 
+ * @author Loïc Vandenberghe (257742)
+ * @author Yann Vonlanthen(258857)
+ *
+ */
+public class RandomGame {
+    
+    //Constants used for convenience
+    private static final Block __ = Block.FREE;
+    private static final Block XX = Block.INDESTRUCTIBLE_WALL;
+    private static final Block xx = Block.DESTRUCTIBLE_WALL;
+    
+    private static final Board board = Board.ofQuadrantNWBlocksWalled(
+            Arrays.asList(Arrays.asList(__, __, __, __, __, xx, __),
+                    Arrays.asList(__, XX, xx, XX, xx, XX, xx),
+                    Arrays.asList(__, xx, __, __, __, xx, __),
+                    Arrays.asList(xx, XX, __, XX, XX, XX, XX),
+                    Arrays.asList(__, xx, __, xx, __, __, __),
+                    Arrays.asList(xx, XX, xx, XX, xx, XX, __)));
+    
+    private static final List<Player> players= Arrays.asList(
+            new Player(PlayerID.PLAYER_1,3,new Cell(1,1),3,3),
+            new Player(PlayerID.PLAYER_2,3,new Cell(7,6),3,3),
+            new Player(PlayerID.PLAYER_3,3,new Cell(8,9),3,3),
+            new Player(PlayerID.PLAYER_4,3,new Cell(3,4),3,3)
+            );
+
+    private RandomGame() {
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        
+        //1) create new game
+        GameState game = new GameState(board, players);
+        
+        //2) prepare randomly generated stuff
+        RandomEventGenerator RANDOM = new RandomEventGenerator(2016, 30, 100);
+        
+        //3) make the game evolve and display it (new tick all 50ms)
+        while(!game.isGameOver()){
+            GameStatePrinter.printGameState(game);
+            game = game.next(RANDOM.randomSpeedChangeEvents(), RANDOM.randomBombDropEvents());
+            Thread.sleep(50);
+            //new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        }
+        
+        
+        
+    }
+
+}
