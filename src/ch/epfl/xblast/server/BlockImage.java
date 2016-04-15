d23bf6d7f5a32702956152734b37ddc3c883a952
LoicVandenberghe <loic.vandenberghe@epfl.ch>
2016-04-15 11:00:20 +0200

    Adding Enum BlockImage

---
+++ src/ch/epfl/xblast/server/BlockImage.java
@@ -0,0 +1,11 @@
+package ch.epfl.xblast.server;
+
+/**
+ * @author Loïc Vandenberghe (257742)
+ * @author Yann Vonlanthen (258857)
+ *
+ */
+public enum BlockImage {
+    IRON_FLOOR, IRON_FLOOR_S, DARK_BLOCK, EXTRA, EXTRA_O, BONUS_BOMB, BONUS_RANGE;
+    
+}
