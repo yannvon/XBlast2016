9a56e1207eca1fd82d1bc6144e4d0acf5a4ced97
LoicVandenberghe <loic.vandenberghe@epfl.ch>
2016-04-15 11:29:27 +0200

    going to Corbu

---
+++ src/ch/epfl/xblast/server/ExplosionPainter.java
@@ -0,0 +1,15 @@
+package ch.epfl.xblast.server;
+
+/**
+ * @author Loïc Vandenberghe (257742)
+ * @author Yann Vonlanthen (258857)
+ *
+ */
+public final class ExplosionPainter {
+    
+    private ExplosionPainter(){}
+    
+    public static byte byteForBomb(){
+        return 0;
+    }
+}
