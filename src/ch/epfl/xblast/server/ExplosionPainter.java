41e5da248a0aa58f00857e0e722df9edd2431ac7
LoicVandenberghe <loic.vandenberghe@epfl.ch>
2016-04-15 15:39:54 +0200

    complete methods in ExplosionPainter

--- src/ch/epfl/xblast/server/ExplosionPainter.java
+++ src/ch/epfl/xblast/server/ExplosionPainter.java
@@ -7,7 +7,7 @@ package ch.epfl.xblast.server;
  */
 public final class ExplosionPainter {
     
-    public static final byte BYTE_FOR_EMPTY = 0b1000;//16?
+    public static final byte BYTE_FOR_EMPTY = 0b10000;//16?
     
     private ExplosionPainter(){}
     
@@ -16,8 +16,8 @@ public final class ExplosionPainter {
      * @return
      */
     public static byte byteForBomb(Bomb bomb){
-        //TODO
-        return 0;
+        boolean white = Integer.bitCount(bomb.fuseLength())==1;
+        return (byte) (white? 21 : 20) ;
     }
     
     /**
@@ -28,8 +28,7 @@ public final class ExplosionPainter {
      * @return
      */
     public static byte byteForBlast(boolean n,boolean e,boolean s,boolean w){
-        //TODO
         
-        return 0;
+        return (byte) ((n? 0b1000:0) | (e? 0b0100:0) | (s? 0b0010:0)| (w? 0b0001:0));
     }
 }
