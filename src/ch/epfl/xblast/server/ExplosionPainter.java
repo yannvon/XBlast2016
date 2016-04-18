1e3a42c8945003427ec6b75593241683dc2748c2
LoicVandenberghe <loic.vandenberghe@epfl.ch>
2016-04-18 12:50:26 +0200

    Add comment in ExplosionPainter

--- src/ch/epfl/xblast/server/ExplosionPainter.java
+++ src/ch/epfl/xblast/server/ExplosionPainter.java
@@ -1,6 +1,8 @@
 package ch.epfl.xblast.server;
 
 /**
+ * Non-instanciable class use to paint bomb and blasts
+ * 
  * @author Loïc Vandenberghe (257742)
  * @author Yann Vonlanthen (258857)
  *
@@ -12,8 +14,10 @@ public final class ExplosionPainter {
     private ExplosionPainter(){}
     
     /**
+     * give the image to paint a given bomb
      * @param bomb
-     * @return
+     *          the bomb to paint
+     * @return the byte corresponding to the image of the bomb
      */
     public static byte byteForBomb(Bomb bomb){
         boolean white = Integer.bitCount(bomb.fuseLength())==1;
@@ -21,11 +25,16 @@ public final class ExplosionPainter {
     }
     
     /**
+     * give the image of a blast according to neighbor blasted cell
      * @param n
+     *      North neighbor cell is blasted?
      * @param e
+     *      East neighbor cell is blasted?
      * @param s
+     *      South neighbor cell is blasted?
      * @param w
-     * @return
+     *      West neighbor cell is blasted?
+     * @return the byte corresponding to the image of a blast
      */
     public static byte byteForBlast(boolean n,boolean e,boolean s,boolean w){
         
