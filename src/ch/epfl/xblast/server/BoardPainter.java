36008b9b6e91b7b72609ecaa7d969ef4efb7ad76
LoicVandenberghe <loic.vandenberghe@epfl.ch>
2016-04-18 12:40:38 +0200

    ADD comment

--- src/ch/epfl/xblast/server/BoardPainter.java
+++ src/ch/epfl/xblast/server/BoardPainter.java
@@ -18,9 +18,13 @@ public final class BoardPainter {
     private final Map<Block,BlockImage> palette;
     private final BlockImage shadow;
     
-    /**TODO
+    /**
+     * constructor 
      * @param palette
+     *          associate each block to an image
      * @param shadow
+     *          image use for a shadow
+     *      
      */
     public BoardPainter(Map<Block,BlockImage> palette, BlockImage shadow){
         this.palette = Collections.unmodifiableMap(
@@ -32,14 +36,16 @@ public final class BoardPainter {
     /**
      * give the corresponding image number to represent the block at the given Cell on the board
      * @param board
+     *          the board to paint
      * @param cell
+     *          the cell in board to paint
      * @return the byte corresponding to the image representing the Cell
      */
     public byte byteForCell(Board board, Cell cell){
         
         Block b= board.blockAt(cell);
         boolean isShadow = b.isFree() && board.blockAt(cell.neighbor(Direction.W)).castsShadow();
-        BlockImage image =  isShadow? palette.get(b) : shadow ;
+        BlockImage image =  isShadow? shadow : palette.get(b);
         
         return (byte) image.ordinal();
     }
