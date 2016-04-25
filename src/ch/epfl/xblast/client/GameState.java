05311ff210694d45565e1f1b91181008c5e5266c
LoicVandenberghe <loic.vandenberghe@epfl.ch>
2016-04-25 14:33:47 +0200

    Add GameState in client and create GameStateDeserialized

---
+++ src/ch/epfl/xblast/client/GameState.java
@@ -0,0 +1,61 @@
+package ch.epfl.xblast.client;
+
+import java.awt.Image;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+import ch.epfl.xblast.ArgumentChecker;
+import ch.epfl.xblast.PlayerID;
+import ch.epfl.xblast.SubCell;
+
+
+/**
+ * @author Loïc Vandenberghe (257742)
+ * @author Yann Vonlanthen (258857)
+ *
+ */
+public final class GameState {
+    
+    public static final class Player {
+        /*
+         * Attributes
+         */
+        private final PlayerID playerId;
+        private final int lives;
+        private final SubCell position;
+        private final Image image;
+        
+        
+        public Player(PlayerID playerId, int lives, SubCell position, Image image){
+            this.playerId=Objects.requireNonNull(playerId);
+            this.lives= ArgumentChecker.requireNonNegative(lives);
+            this.position=Objects.requireNonNull(position);
+            this.image=image;//TODO requireNonNull?
+        }
+        
+    }
+    
+    /*
+     * Attributes
+     */
+    private final List<Player> players;
+    private final List<Image> board ,explosions, scores, time;
+
+    /**
+     * @param players
+     * @param board
+     *          TODO precise reading order
+     * @param explosions
+     * @param scores
+     * @param time
+     */
+    public GameState(List<Player> players, List<Image> board, List<Image> explosions,List<Image> scores, List<Image> time){
+        this.players = Collections.unmodifiableList(new ArrayList<>(Objects.requireNonNull(players)));
+        this.board=Collections.unmodifiableList(new ArrayList<>(Objects.requireNonNull(board)));
+        this.explosions=Collections.unmodifiableList(new ArrayList<>(Objects.requireNonNull(explosions)));
+        this.scores=Collections.unmodifiableList(new ArrayList<>(Objects.requireNonNull(scores)));
+        this.time=Collections.unmodifiableList(new ArrayList<>(Objects.requireNonNull(time)));
+    }
+}
---
+++ src/ch/epfl/xblast/client/GameStateDeserializer.java
@@ -0,0 +1,24 @@
+package ch.epfl.xblast.client;
+
+import java.util.List;
+
+/**
+ * Non-instanciable class offering static method to deserialize the GameSate to the client.
+ * @author Loïc Vandenberghe (257742)
+ * @author Yann Vonlanthen (258857)
+ *
+ */
+public final class GameStateDeserializer {
+
+    private GameStateDeserializer(){}
+    
+    public static GameState deserializeGameState(List<Byte> serialized){
+        
+        
+        
+        
+        
+        return null;
+    }
+    
+}
