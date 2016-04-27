a92e07063805d0bd874347bb21a8a5f888fd70a3
Yann Vonlanthen <yann.vonlanthen@epfl.ch>
2016-04-27 11:24:48 +0200

    Changes to ImageCollection.

--- src/ch/epfl/xblast/client/ImageCollection.java
+++ src/ch/epfl/xblast/client/ImageCollection.java
@@ -11,6 +11,9 @@ import java.util.NoSuchElementException;
 import javax.imageio.ImageIO;
 
 /**
+ * This immutable class represents a collection of images that are provided by
+ * repository and indexed by an integer value.
+ * 
  * @author Loïc Vandenberghe (257742)
  * @author Yann Vonlanthen (258857)
  *
@@ -18,48 +21,65 @@ import javax.imageio.ImageIO;
 public final class ImageCollection {
     
     private final Map<Byte,Image> images;
-    
-    /**
-     * @param repository
-     * @throws URISyntaxException 
-     */
-    public ImageCollection(String repository) throws URISyntaxException{    //FIXME throw or try/catch?
-        // don't modify
-        Map<Byte, Image> images = new HashMap<>();
-        File directory = new File(ImageCollection.class.getClassLoader()
-                .getResource(repository).toURI());
 
-        for (File f : directory.listFiles()) {
-            try {
-                Byte b = Byte.parseByte(f.getName().substring(0, 3));
-                images.put(b, ImageIO.read(f));
-            } catch (Exception e) {
-                // do nothing
+    /**
+     * Only Constructor, taking the name of the repository that contains the
+     * images as argument.
+     * 
+     * @param repository
+     *            name of the repository where the images are saved
+     */
+    public ImageCollection(String repository) { // FIXME error handling!
+
+        Map<Byte, Image> images = new HashMap<>();
+        try {
+            // don't modify the following line!
+            File directory = new File(ImageCollection.class
+                                        .getClassLoader()
+                                        .getResource(repository)
+                                        .toURI());
+
+            for (File f : directory.listFiles()) {
+                try {
+                    Byte b = Byte.parseByte(f.getName().substring(0, 3));
+                    images.put(b, ImageIO.read(f));
+                } catch (Exception e) {
+                    // do nothing, we don't want to abort the operation if one image fails to load.
+                }
             }
+        } catch (URISyntaxException e) {
+            throw new Error(
+                    "The directory named " + repository + " doesn't exist!");
         }
         this.images = Collections.unmodifiableMap(images);
-        
-    }
-    
-    /**
-     * @param imageNumber
-     * @return
-     */
-    public Image image(byte imageNumber){
-        Image im=imageOrNull(imageNumber);
-        if (im ==null)
-            throw new NoSuchElementException();
-        return im;
-    }
-    
-    
-    /**
-     * @param imageNumber
-     * @return
-     */
-    public Image imageOrNull(byte imageNumber){
-        
-       return images.get(imageNumber);
     }
 
+    /**
+     * Method allowing to get an image for a specific byte value.
+     * 
+     * @param imageNumber
+     *            byte value characterising an image
+     * @return the image that corresponds to the byte value
+     * @throws NoSuchElementException
+     *             if no image corresponds to given index.
+     */
+    public Image image(byte imageNumber) {
+        Image i = imageOrNull(imageNumber);
+        if (i == null)
+            throw new NoSuchElementException();
+        return i;
+    }
+
+    /**
+     * Method allowing to get an image for a specific byte value.
+     * 
+     * @param imageNumber
+     *            byte value characterising an image
+     * @return the image that corresponds to the byte value or null if no image
+     *         corresponds to given index.
+     */
+    public Image imageOrNull(byte imageNumber) {
+
+        return images.get(imageNumber);
+    }
 }
