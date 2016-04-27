e3f1ee327913c12038546c842efbe007d7bd48b6
Yann Vonlanthen <yann.vonlanthen@epfl.ch>
2016-04-27 11:41:11 +0200

    changes in Level, adding requireNonNull in constructor!

--- src/ch/epfl/xblast/server/Level.java
+++ src/ch/epfl/xblast/server/Level.java
@@ -4,12 +4,13 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import ch.epfl.xblast.Cell;
 import ch.epfl.xblast.PlayerID;
 
 /**
- * class that represent a level with a initial gameState and a BoardPainter
+ * Class that represents a level by an initial GameState and a BoardPainter.
  * 
  * @author Loïc Vandenberghe (257742)
  * @author Yann Vonlanthen (258857)
@@ -20,29 +21,36 @@ public final class Level {
     /*
      * DEFAULT LEVEL
      */
-    public static final Level DEFAULT_LEVEL = new Level(
-            defaultBoardPainter(), defaultGameState());
-    
+    public static final Level DEFAULT_LEVEL = new Level(defaultBoardPainter(),
+            defaultGameState());
+
     /**
      * ADDITIONAL Static method that constructs a classic BoardPainter that uses
      * the standard images.
      * 
-     * @return a classic BoardPainter
+     * @return a default BoardPainter
      */
     private static BoardPainter defaultBoardPainter(){
+        
         Map<Block,BlockImage> palette = new HashMap<>();
+        
         palette.put(Block.INDESTRUCTIBLE_WALL,BlockImage.DARK_BLOCK);
         palette.put(Block.FREE,BlockImage.IRON_FLOOR);
         palette.put(Block.DESTRUCTIBLE_WALL,BlockImage.EXTRA);
         palette.put(Block.CRUMBLING_WALL,BlockImage.EXTRA_O);
         palette.put(Block.BONUS_RANGE,BlockImage.BONUS_RANGE);
         palette.put(Block.BONUS_BOMB,BlockImage.BONUS_BOMB);
-
+        
         return new BoardPainter(palette, BlockImage.IRON_FLOOR_S);
     }
     
     /**
-     * ADDITIONAL Static method that constructs the default initial GameState.
+     * ADDITIONAL Static method that constructs the default initial GameState,
+     * consisting of the default Board and the default player configuration.
+     * 
+     * The default player configuration places all players in the corner and the
+     * players start with 3 lives, a bomb range of 3 and 2 maximal bombs.
+     * 
      * @return a default initial GameState
      */
     private static  GameState defaultGameState(){
@@ -59,32 +67,38 @@ public final class Level {
                 Arrays.asList(xx, XX, xx, XX, xx, XX, __)));
         
         List<Player> players = Arrays.asList(
-                new Player(PlayerID.PLAYER_1,3,new Cell(1,1),2,3),
-                new Player(PlayerID.PLAYER_2,3,new Cell(13,1),2,3),
-                new Player(PlayerID.PLAYER_3,3,new Cell(13,11),2,3),
-                new Player(PlayerID.PLAYER_4,3,new Cell(1,11),2,3)
-                );
+                new Player(PlayerID.PLAYER_1, 3, new Cell( 1, 1), 2, 3),
+                new Player(PlayerID.PLAYER_2, 3, new Cell(13, 1), 2, 3),
+                new Player(PlayerID.PLAYER_3, 3, new Cell(13,11), 2, 3),
+                new Player(PlayerID.PLAYER_4, 3, new Cell( 1,11), 2, 3));
         
         return new GameState(board, players);
     }
-    
+
     /*
      * Attributes
      */
     private final BoardPainter boardPainter;
     private final GameState initialGameState;
-    
+
     /**
-     * Constructor
+     * Constructor taking a BoardPainter and an initial GameState as arguments.
+     * 
      * @param boardPainter
+     *            the BoardPainter used for this level
      * @param initialGame
+     *            the GameState at the start of the game
+     * @throws NullPointerException
+     *             if one of the arguments are null
      */
     public Level(BoardPainter boardPainter, GameState initialGame) {
-        this.boardPainter= boardPainter;
-        this.initialGameState=initialGame;
+        this.boardPainter = Objects.requireNonNull(boardPainter);
+        this.initialGameState = Objects.requireNonNull(initialGame);
     }
 
     /**
+     * Returns the BoardPainter of this Level
+     * 
      * @return the boardPainter of the Level
      */
     public BoardPainter boardPainter() {
@@ -92,7 +106,9 @@ public final class Level {
     }
 
     /**
-     * @return the gameState at the beginning of the game 
+     * Returns the initial GameState of the Level
+     * 
+     * @return the initial GameState
      */
     public GameState initialGameState() {
         return initialGameState;
