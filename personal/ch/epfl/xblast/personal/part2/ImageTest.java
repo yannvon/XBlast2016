7020c5a711ec8e89c553b90cd59f78efaa6ffe33
LoicVandenberghe <loic.vandenberghe@epfl.ch>
2016-04-27 13:18:31 +0200

    add imageChargeTest

---
+++ personal/ch/epfl/xblast/personal/part2/ImageTest.java
@@ -0,0 +1,15 @@
+package ch.epfl.xblast.personal.part2;
+
+import org.junit.Test;
+
+import ch.epfl.xblast.client.ImageCollection;
+
+public class ImageTest {
+
+    @Test
+    public void chargedTest() {
+        ImageCollection images= new ImageCollection("player");
+        
+    }
+
+}
