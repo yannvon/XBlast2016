ae00b3c85a25377bed1117c1220219c739da9bf1
Yann Vonlanthen <yann.vonlanthen@epfl.ch>
2016-04-28 21:46:35 +0200

    Important changes to RunLengthEncoder

--- src/ch/epfl/xblast/RunLengthEncoder.java
+++ src/ch/epfl/xblast/RunLengthEncoder.java
@@ -1,7 +1,6 @@
 package ch.epfl.xblast;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -14,30 +13,45 @@ import java.util.List;
  *
  */
 public final class RunLengthEncoder {
+    private RunLengthEncoder() {
+    }
 
     /*
      * Constants
      */
-    private static int LONGEST_RUN = 130;
-    
+    private static final int LONGEST_RUN = 130;
+
     /**
-     * TODO
+     * Takes a list of Bytes as argument and returns an run length encoded list.
+     * 
+     * The technique used is a slightly improved version of the usual run length
+     * encoding: It consists of keeping the bytes how they are if they don't
+     * occur more than twice in a row. Otherwise a negative number is added in
+     * front of the compressed byte. The absolute value of the indicator byte
+     * plus two corresponds to the amount of occurrences of said byte.
+     * 
+     * This technique requires that the given list of byte does possess any
+     * negative byte.
+     * 
      * @param l
-     * @return
+     *            list of bytes
+     * @return list of compressed bytes (see explanation above)
+     * @throws IllegalArgumentException
+     *             if the received list contains a negative byte
      */
-    public static List<Byte> encode(List<Byte> l){
+    public static List<Byte> encode(List<Byte> l) {
         List<Byte> output = new ArrayList<>();
-        
         byte lastByte = 0;
         int count = 0;
-        
-        for(Byte b : l){
-            if(b < 0)
-                throw new IllegalArgumentException("Cannot encode a negative byte!");
-            
-            if(b == lastByte && count < LONGEST_RUN){
+
+        for (Byte b : l) {
+            if (b < 0)
+                throw new IllegalArgumentException(
+                        "Cannot encode a negative byte!");
+            // since we can only use negative byte values, we can only encode
+            // 130 consecutive occurrences at once.
+            if (b == lastByte && count < LONGEST_RUN)
                 count++;
-            }
             else {
                 output.addAll(encodedBytes(count, lastByte));
                 lastByte = b;
@@ -45,43 +59,64 @@ public final class RunLengthEncoder {
             }
         }
         output.addAll(encodedBytes(count, lastByte));
-        
+
         return output;
     }
-    
+
     /**
-     * TODO
+     * Decodes a list of bytes that was encoded using the run length encoding
+     * described at the {@link #encode(List)} method.
+     * 
      * @param l
-     * @return
+     *            list of run length encoded bytes
+     * @return decoded version of the list of bytes
+     * @throws IllegalArgumentException
+     *             if the last element of given list is negative
      */
-    public static List<Byte> decode(List<Byte> l){
-        List<Byte> output = new ArrayList<>();
+    public static List<Byte> decode(List<Byte> l) {
+        if(l.get(l.size()) > 0)                         //FIXME on avait oublié!
+            throw new IllegalArgumentException();
         
+        List<Byte> output = new ArrayList<>();
         int n = 1;
-        for(Byte b : l){
-            if(b >= 0){
+        for (Byte b : l) {
+            if (b >= 0) {
                 output.addAll(Collections.nCopies(n, b));
                 n = 1;
-            }
-            else{
+            } else {
                 n = -b + 2;
             }
         }
         return output;
     }
-    
-    private static List<Byte> encodedBytes(int count, byte lastByte){
-        List<Byte> newBytes = new ArrayList<>();
-        
-        if (count <= 2 )
-            newBytes.addAll(Collections.nCopies(count, lastByte));
-        else{
-            newBytes.add((byte) -(count - 2));
-            newBytes.add(lastByte);
-        }
-        return newBytes;
-    }
-    
 
-    private RunLengthEncoder() {}
+    /**
+     * Additional private method that returns the correct encoding for a given
+     * pair. The pair consists of a (positive) byte value that has to be encoded
+     * and an integer that represents the number of consecutive occurrences of
+     * said byte.
+     * 
+     * The returned list consists of maximum two bytes, the exacted technique
+     * used is described at {@link #encode(List)} the method.
+     * 
+     * Careful: the count argument is not allowed to succeed LONGEST_RUN!   FIXME throw exception?
+     * 
+     * @param count
+     *            number of consecutive occurrences of following byte
+     * @param b
+     *            byte that is encoded
+     * @return list consisting of maximum two bytes representing a number of
+     *         occurrences of given byte
+     */
+    private static List<Byte> encodedBytes(int count, byte b) {
+        List<Byte> encoded = new ArrayList<>();
+
+        if (count <= 2)
+            encoded.addAll(Collections.nCopies(count, b));
+        else {
+            encoded.add((byte) -(count - 2));
+            encoded.add(b);
+        }
+        return encoded;
+    }
 }
