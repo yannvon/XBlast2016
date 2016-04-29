a9679c3820198824ca93bf965d62aaecfa1ab36c
LoicVandenberghe <loic.vandenberghe@epfl.ch>
2016-04-29 12:31:07 +0200

    add some test

--- personal/ch/epfl/xblast/personal/part2/ImageTest.java
+++ personal/ch/epfl/xblast/personal/part2/ImageTest.java
@@ -1,5 +1,10 @@
 package ch.epfl.xblast.personal.part2;
 
+import static org.junit.Assert.*;
+
+import java.awt.Image;
+import java.util.NoSuchElementException;
+
 import org.junit.Test;
 
 import ch.epfl.xblast.client.ImageCollection;
@@ -9,7 +14,20 @@ public class ImageTest {
     @Test
     public void chargedTest() {
         ImageCollection images= new ImageCollection("player");
-        
+        Image c= images.image((byte) 0);
+    }
+    
+    @Test(expected=NoSuchElementException.class)
+    public void ExceptionTest() {
+        ImageCollection images= new ImageCollection("player");
+        Image c= images.image((byte) 74);
+    }
+    
+    @Test
+    public void nullTest() {
+        ImageCollection images= new ImageCollection("player");
+        Image c= images.imageOrNull((byte) 74);
+        assertEquals(null,c);
     }
 
 }
