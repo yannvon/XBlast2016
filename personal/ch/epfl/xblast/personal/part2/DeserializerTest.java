0e719103aed7423f29cc8c202dee42d1d0bc7ac3
Yann Vonlanthen <yann.vonlanthen@epfl.ch>
2016-05-02 14:54:18 +0200

    addition of 2 (useless) tests

--- personal/ch/epfl/xblast/personal/part2/DeserializerTest.java
+++ personal/ch/epfl/xblast/personal/part2/DeserializerTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.Test;
@@ -24,6 +25,20 @@ public class DeserializerTest {
     }
     
     @Test
+    (expected = NullPointerException.class)
+    public void gameStateNullPointer(){
+        GameState game = new GameState(null, Collections.EMPTY_LIST, Collections.EMPTY_LIST, Collections.EMPTY_LIST, Collections.EMPTY_LIST);
+        
+    }
+    
+    @Test
+    (expected = IllegalArgumentException.class)
+    public void gameStateThrowsIllegal(){
+        GameState game = new GameState(Collections.EMPTY_LIST, null, Collections.EMPTY_LIST, Collections.EMPTY_LIST, Collections.EMPTY_LIST);
+    }
+    
+    
+    @Test
     public void decodeTest(){
         List<Integer> codei= Arrays.asList(-4,2,-1,4,5,6,34,-5,5,-2,3,7,4,12,-3,22);
         List<Byte> code=new ArrayList<>();
@@ -33,9 +48,10 @@ public class DeserializerTest {
         List<Integer> ex = Arrays.asList(2,2,2,2,2,2,4,4,4,5,6,34,5,5,5,5,5,5,5,3,3,3,3,7,4,12,22,22,22,22,22);
         List<Byte> expected = new ArrayList<>();
         ex.forEach(x->expected.add((byte)x.intValue()));
-        System.out.println(expected);
-        System.out.println(decode);
+        //System.out.println(expected);
+        //System.out.println(decode);
         assertEquals(expected, decode);
+        
     }
 
 }
