eb89ba4e604204549abd841725ec70156d0eff7a
Yann Vonlanthen <yann.vonlanthen@epfl.ch>
2016-05-04 16:51:02 +0200

    adding PlayerAction enum

---
+++ src/ch/epfl/xblast/PlayerAction.java
@@ -0,0 +1,12 @@
+package ch.epfl.xblast;
+
+/**
+ * Enumeration of actions that the physical player has.
+ * 
+ * @author Loïc Vandenberghe (257742)
+ * @author Yann Vonlanthen (258857)
+ *
+ */
+public enum PlayerAction {
+    JOIN_GAME, MOVE_N, MOVE_E, MOVE_S, MOVE_W, STOP, DROP_BOMB,
+}
