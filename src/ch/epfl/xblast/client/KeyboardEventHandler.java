0d91b5b8ed524baf806a34c49f7e754a7c53d272
Yann Vonlanthen <yann.vonlanthen@epfl.ch>
2016-05-04 16:59:48 +0200

    adding KeyboardEventHandler class

---
+++ src/ch/epfl/xblast/client/KeyboardEventHandler.java
@@ -0,0 +1,17 @@
+package ch.epfl.xblast.client;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyListener;
+
+/**
+ * This class represents a Listener of Keyboard events.
+ * 
+ * @author Loïc Vandenberghe (257742)
+ * @author Yann Vonlanthen (258857)
+ *
+ */
+public final class KeyboardEventHandler extends KeyAdapter
+        implements KeyListener {    //FIXME final?
+    
+
+}
