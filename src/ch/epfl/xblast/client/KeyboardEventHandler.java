d4f7389f2602dee2ac794e5d8c991df3d1b70299
Yann Vonlanthen <yann.vonlanthen@epfl.ch>
2016-05-06 13:11:24 +0200

    comments in KeyboardEventHandler

--- src/ch/epfl/xblast/client/KeyboardEventHandler.java
+++ src/ch/epfl/xblast/client/KeyboardEventHandler.java
@@ -25,10 +25,17 @@ public final class KeyboardEventHandler extends KeyAdapter
      */
     private final Map<Integer, PlayerAction> controls;
     private final Consumer<PlayerAction> consumer;
-    
+
     /**
+     * Sole constructor taking the mapping between a key pressed and the action
+     * to take, and a consumer as parameter.
+     * 
      * @param controls
+     *            map associating the player actions to the key's codes.
      * @param consumer
+     *            a consumer of PlayerActions
+     * @throws NullPointerException
+     *             if one of the arguments was null.
      */
     public KeyboardEventHandler(Map<Integer, PlayerAction> controls,
             Consumer<PlayerAction> consumer) {
@@ -39,7 +46,11 @@ public final class KeyboardEventHandler extends KeyAdapter
     
     
     @Override
-    public void keyPressed(KeyEvent e){
+    /**
+     * If a key that is present in the map is pressed, the consumer receives the
+     * PlayerAction that was taken.
+     */
+    public void keyPressed(KeyEvent e) {
         int keyCode = e.getKeyCode();
         if(controls.containsKey(keyCode))
             consumer.accept(controls.get(keyCode));
