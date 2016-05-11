7b2031ee834296debc1ce84fe86d9f9d2164c753
LoicVandenberghe <loic.vandenberghe@epfl.ch>
2016-05-11 13:54:02 +0200

    conflicts corrected

--- src/ch/epfl/xblast/server/Main.java
+++ src/ch/epfl/xblast/server/Main.java
@@ -6,14 +6,13 @@ import java.net.SocketAddress;
 import java.net.StandardProtocolFamily;
 import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.Set;
-
-import com.sun.org.apache.bcel.internal.generic.NEWARRAY;
 
 import ch.epfl.xblast.Direction;
 import ch.epfl.xblast.PlayerAction;
@@ -30,6 +29,7 @@ public class Main {
     /*
      * Constants
      */
+    private static final Level LEVEL = Level.DEFAULT_LEVEL;//FIXME or level?    
     private static final int DEFAULT_NUMBER_OF_CLIENT = 4;    
     private static final SocketAddress PORT_ADDRESS= new InetSocketAddress(2016);
     
@@ -37,32 +37,35 @@ public class Main {
     /**
      * Main method of the server
      * @param args
-     *          Determine the number of client to connect before the game begin
+     *          Determine the number of client to connect before the game begins.
      * @throws IOException 
      */
     public static void main(String[] args) throws IOException { //FIXME exception?
+        
         /*
          * Phase 1
          */
-        int numberOfClient = (args.length!=0)?Integer.parseInt(args[0]):DEFAULT_NUMBER_OF_CLIENT ;
-        if(numberOfClient > 4)
-            numberOfClient=4; //FIXME constant
-        
+        int numberOfClients = (args.length != 0) ? Integer.parseInt(args[0])
+                : DEFAULT_NUMBER_OF_CLIENT;
+        if (numberOfClients > 4)
+            numberOfClients = 4; // FIXME constant OR throw error?
+ 
         DatagramChannel channel = DatagramChannel.open(StandardProtocolFamily.INET);
         channel.bind(PORT_ADDRESS);
         channel.configureBlocking(true);
-        Map<SocketAddress,PlayerID> address = new HashMap<>();//TODO player->address or adress->player
-        ByteBuffer buffer = ByteBuffer.allocate(1);
-        
+        Map<SocketAddress, PlayerID> clientAdresses = new HashMap<>();
+        ByteBuffer oneByteBuffer = ByteBuffer.allocate(1);
+
         /*
          * add the client who want to join the game
          */
-        while(address.size()!=numberOfClient){
-            SocketAddress senderAddress = channel.receive(buffer);
-            if(buffer.get(0)==PlayerAction.JOIN_GAME.ordinal()){
-                address.put(senderAddress,PlayerID.values()[address.size()]);
-                buffer.reset();
+        while (clientAdresses.size() != numberOfClients) {
+            SocketAddress senderAddress = channel.receive(oneByteBuffer);
+            if (oneByteBuffer.get(0) == PlayerAction.JOIN_GAME.ordinal()) {    //get()
+                clientAdresses.put(senderAddress,
+                        PlayerID.values()[clientAdresses.size()]);
             }
+            oneByteBuffer.reset();
         }
         
         
@@ -70,15 +73,30 @@ public class Main {
         /*
          * Phase 2
          */
-        GameState game = Level.DEFAULT_LEVEL.initialGameState();      
-        channel.configureBlocking(false);
+        GameState game = LEVEL.initialGameState();
+        channel.configureBlocking(false);       //TODO comment
         
         while(!game.isGameOver()){
+            // 1) send current GameState to clients
+            // 1.1) serialize GameState
+            List<Byte> serialized = GameStateSerializer.serialize(LEVEL.boardPainter(), game);
+            ByteBuffer gameStateBuffer = ByteBuffer.allocate(serialized.size());
+            gameStateBuffer.put((byte) 0);  //TODO explain 
+            serialized.forEach(gameStateBuffer::put);
+
+            for(Entry<SocketAddress, PlayerID> e : clientAdresses.entrySet()){
+                gameStateBuffer.put(0, (byte) e.getValue().ordinal());  //FIXME move tampon?
+                gameStateBuffer.flip(); //FIXME if possible put out of loop
+                channel.send(gameStateBuffer, e.getKey());
+            }
+
+            
             Map<PlayerID,PlayerAction> actions= new HashMap<>();
-            while(channel.receive(buffer)!=null){
+            while(channel.receive(oneByteBuffer)!=null){
                 //TODO?????
             }
             
+            // 2) get client input
             Map<PlayerID,Optional<Direction>> mouvements= new HashMap<>();
             Set<PlayerID> bombDrpEvent = new HashSet<>();
             
