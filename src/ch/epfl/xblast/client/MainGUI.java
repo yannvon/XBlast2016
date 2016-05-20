b80122479a3bca4108c5ab52ec2b6ba1dd82e752
LoicVandenberghe <loic.vandenberghe@epfl.ch>
2016-05-20 20:57:59 +0200

    improve MainGUI

--- src/ch/epfl/xblast/client/MainGUI.java
+++ src/ch/epfl/xblast/client/MainGUI.java
@@ -77,93 +77,6 @@ public class MainGUI {
         while(!input){
             Thread.sleep(50);
         }
-        
-        /*
-         * PHASE 1
-         * 1.1) retrieve IP-address and open channel
-         */
-        serverAddress = new InetSocketAddress(hostName, PORT);
-        
-        //FIXME try with resources. HOW?
-        channel = DatagramChannel.open(StandardProtocolFamily.INET);
-        
-        //TODO comments
-        channel.configureBlocking(false);
-
-        /*
-         * 1.2) Send request to join game to Server as long as the server
-         * doesn't send the initial GameState.
-         */
-        ByteBuffer sendByteBuffer = ByteBuffer.allocate(1);
-        ByteBuffer receiveByteBuffer = ByteBuffer.allocate(MAX_RECEIVING_BYTES);
-        sendByteBuffer.put((byte) PlayerAction.JOIN_GAME.ordinal());
-        sendByteBuffer.flip();
-        do{
-            channel.send(sendByteBuffer, serverAddress);
-            sendByteBuffer.rewind();
-            Thread.sleep(GAME_JOIN_REQUEST_REPEATING_TIME);
-        }while((channel.receive(receiveByteBuffer)) == null);
-        
-        /*
-         * PHASE 2
-         * 2.1) Start by invoking the parallel EDT Swing thread.
-         */
-        SwingUtilities.invokeAndWait(() -> createUI());
-        
-        
-        // From now one the client will wait to get the next GameState from the server.
-        channel.configureBlocking(true);
-
-        /*
-         * 2.2) As long as the program runs the client waits for a new GameState
-         * and shares it with the parallel Swing thread.
-         */
-        do{
-            receiveByteBuffer.flip();
-            PlayerID id = PlayerID.values()[receiveByteBuffer.get()];  //FIXME do this everytime? /check/throw exception?
-            List<Byte> serialized = new ArrayList<>();
-            while(receiveByteBuffer.hasRemaining()){
-                serialized.add(receiveByteBuffer.get());
-            }
-            GameState gameState = GameStateDeserializer.deserializeGameState(serialized);
-            xbc.setGameState(gameState, id);
-            receiveByteBuffer.clear();
-            channel.receive(receiveByteBuffer);
-        } while (true);
-        //FIXME close channel?
-    }
-
-    public static void createUI(){
-        
-        /*
-         * Open new Window.
-         */
-        JFrame f = new JFrame("XBlast 2016");
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        xbc = new XBlastComponent();
-        
-        f.getContentPane().add(xbc);
-        f.setResizable(false);
-        f.pack();
-        f.setVisible(true);
-        xbc.requestFocusInWindow();
-
-        /*
-         * Manage the Keyboard input by adding a keyListener to the
-         * XBlastComponent that sends a message to the server when a relevant
-         * key was pressed.
-         */
-        Consumer<PlayerAction> c = (playerAction) -> {
-            ByteBuffer oneByteBuffer = ByteBuffer.allocate(1);
-            oneByteBuffer.put((byte) playerAction.ordinal());
-            oneByteBuffer.flip();
-            try {
-                channel.send(oneByteBuffer, serverAddress);
-            } catch (Exception e) {
-                //Do nothing.
-            }
-        };
-        xbc.addKeyListener(new KeyboardEventHandler(KeyboardEventHandler.DEFAULT_CONTROL_MAP, c));
     }
     
     public static void createMenu(){
@@ -213,31 +126,60 @@ public class MainGUI {
          */
         JButton b1 = new JButton("Local Game");
         b1.addActionListener(e -> {
-            hostName = DEFAULT_HOST;
-            input = true;
+            Runnable localMain = new Runnable() {
+                public void run() {
+                    try {
+                        ch.epfl.xblast.LocalGame.main(new String[0]);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            new Thread(localMain).start();
             f.dispose();});
         
         JButton b2 = new JButton("Enter IP-address");
         b2.addActionListener(e -> {
-            hostName = (String) textboxIP.getValue();
-            input = true;
+            String[] address = { (String) textboxIP.getValue()};
+            Runnable clientMain = new Runnable() {
+                public void run() {
+                    try {
+                        ch.epfl.xblast.client.Main.main(address);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            new Thread(clientMain).start();
             f.dispose();});
         
         JButton b3 = new JButton("Start Server");
         b3.addActionListener(e -> {
-            String[] args = { "1" };
+            String[] argsMain = { "1" }; //TODO
+            String[] argsClient = { DEFAULT_HOST };
             Runnable serverMain = new Runnable() {
                 public void run() {
                     try {
-                        ch.epfl.xblast.server.Main.main(args);
+                        ch.epfl.xblast.server.Main.main(argsMain);
                     } catch (Exception e) {
                         // TODO Auto-generated catch block
                         e.printStackTrace();
                     }
                 }
             };
-
+            Runnable clientMain = new Runnable() {
+                public void run() {
+                    try {
+                        ch.epfl.xblast.client.Main.main(argsClient);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
             new Thread(serverMain).start();
+            new Thread(clientMain).start();
+            f.dispose();
+            
         });
 
         JPanel buttons = new JPanel(new FlowLayout());
