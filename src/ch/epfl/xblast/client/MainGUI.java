d27860264ac2063d033b761e0218701b0ff12e7f
LoicVandenberghe <loic.vandenberghe@epfl.ch>
2016-05-20 22:56:39 +0200

    new disposition for buttons

--- src/ch/epfl/xblast/client/MainGUI.java
+++ src/ch/epfl/xblast/client/MainGUI.java
@@ -1,9 +1,12 @@
 package ch.epfl.xblast.client;
 
 import java.awt.BorderLayout;
+import java.awt.Checkbox;
 import java.awt.Choice;
 import java.awt.Color;
+import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.Label;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
@@ -15,6 +18,7 @@ import java.util.List;
 import java.util.function.Consumer;
 
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFormattedTextField;
@@ -26,6 +30,7 @@ import javax.swing.SwingUtilities;
 import ch.epfl.xblast.Cell;
 import ch.epfl.xblast.PlayerAction;
 import ch.epfl.xblast.PlayerID;
+import javafx.scene.control.CheckBox;
 
 /**
  * Main class of the Client. In charge of communicating with the server and
@@ -66,6 +71,17 @@ public class MainGUI {
     
     public static void createMenu(){
         
+//      //SECOND
+//      JLabel instruction2 = new JLabel("Enter server ip address");
+//      p.add(instruction2, BorderLayout.LINE_START);
+//      
+//      JFormattedTextField textboxServer = new JFormattedTextField();
+//      //add text field
+//      textboxIP.setValue(DEFAULT_HOST);
+//      textboxIP.setColumns(10);
+//      p.add(textboxServer, BorderLayout.CENTER);
+        
+        
         /*
          * Open new Window.
          */
@@ -75,51 +91,18 @@ public class MainGUI {
         /*
          * Title
          */
-        JPanel p = new JPanel(new BorderLayout());
         ImageIcon i = new ImageIcon(GUI_COLLECTION.image((byte) 000));
         JLabel title = new JLabel(i);
         title.setBorder(BorderFactory.createMatteBorder(20, 20, 20, 20, new Color(0, 200, 255)));
-        p.add(title, BorderLayout.PAGE_START);
 
         /*
-         * Input
+         * Local Game
          */
-        //FIRST
-        JLabel instruction1 = new JLabel("Enter server ip address");
-        instruction1.setBorder(BorderFactory.createMatteBorder(10, 10, 10, 0, new Color(0, 170, 255)));
-        p.add(instruction1, BorderLayout.LINE_START);
+        //1)Input TODO different Levels or options
         
-        JFormattedTextField textboxIP = new JFormattedTextField();
-        //add text field
-        textboxIP.setValue(DEFAULT_HOST);
-        textboxIP.setColumns(10);
-        textboxIP.setBorder(BorderFactory.createMatteBorder(10, 0, 10, 10, new Color(0, 170, 255)));
-        p.add(textboxIP, BorderLayout.CENTER);
-        
-//        //SECOND
-//        JLabel instruction2 = new JLabel("Enter server ip address");
-//        p.add(instruction2, BorderLayout.LINE_START);
-//        
-//        JFormattedTextField textboxServer = new JFormattedTextField();
-//        //add text field
-//        textboxIP.setValue(DEFAULT_HOST);
-//        textboxIP.setColumns(10);
-//        p.add(textboxServer, BorderLayout.CENTER);
-        
-        /*
-         * add list for players
-         */
-        Label nombreText = new Label("number of player", Label.RIGHT);
-        Choice nbPlayers = new Choice();
-        for(int n=1; n<=4;n++)
-            nbPlayers.add(new Integer(n).toString());
-        nbPlayers.select(0);
-        
-        /*
-         * Buttons
-         */
-        JButton b1 = new JButton("Local Game");
-        b1.addActionListener(e -> {
+        //2)button
+        JButton localButton = new JButton("Local Game");
+        localButton.addActionListener(e -> {
             Runnable localMain = new Runnable() {
                 public void run() {
                     try {
@@ -132,8 +115,24 @@ public class MainGUI {
             new Thread(localMain).start();
             f.dispose();});
         
-        JButton b2 = new JButton("Enter IP-address");
-        b2.addActionListener(e -> {
+        //3)Panel
+        JPanel localPanel = new JPanel(new BorderLayout());
+        localPanel.add(localButton,BorderLayout.EAST);
+        localPanel.setBorder(BorderFactory.createMatteBorder(10, 10, 10, 10, new Color(0, 0, 255)));
+        
+        /*
+         * Client
+         */
+        //1)Input
+        JLabel instruction1 = new JLabel(" Enter server ip address  :  ",Label.LEFT);//FIXME What the fuck????
+        
+        JFormattedTextField textboxIP = new JFormattedTextField();
+        textboxIP.setValue(DEFAULT_HOST);
+        textboxIP.setColumns(10);
+
+        //2)button
+        JButton clientButton = new JButton("Enter IP-address");
+        clientButton.addActionListener(e -> {
             String[] address = { (String) textboxIP.getValue()};
             Runnable clientMain = new Runnable() {
                 public void run() {
@@ -147,8 +146,31 @@ public class MainGUI {
             new Thread(clientMain).start();
             f.dispose();});
         
-        JButton b3 = new JButton("Start Server");
-        b3.addActionListener(e -> {
+        //3)Panel
+        JPanel clientPanel = new JPanel(new BorderLayout());
+        clientPanel.add(instruction1,BorderLayout.NORTH);
+        clientPanel.add(textboxIP,BorderLayout.CENTER);
+        clientPanel.add(clientButton,BorderLayout.EAST);
+        clientPanel.setBorder(BorderFactory.createMatteBorder(10, 10, 10, 10, new Color(0, 125, 255)));
+        
+        
+        /*
+         * Client
+         */
+        
+        //1)Input        
+        JLabel numberText = new JLabel("Number of player");
+        
+        Choice nbPlayers = new Choice();
+        for(int n=1; n<=4;n++)
+            nbPlayers.add(new Integer(n).toString());
+        nbPlayers.select(0);
+        
+        Checkbox checkLaunchClient = new Checkbox("Launch Client",true);
+        
+        //2)Button
+        JButton serverButtton = new JButton("Start Server");
+        serverButtton.addActionListener(e -> {
             String[] argsMain = { nbPlayers.getSelectedItem() }; //TODO
             String[] argsClient = { DEFAULT_HOST };
             Runnable serverMain = new Runnable() {
@@ -161,34 +183,51 @@ public class MainGUI {
                     }
                 }
             };
-            Runnable clientMain = new Runnable() {
-                public void run() {
-                    try {
-                        ch.epfl.xblast.client.Main.main(argsClient);
-                    } catch (Exception e) {
-                        e.printStackTrace();
+            if(checkLaunchClient.getState()){
+                Runnable clientMain = new Runnable() {
+                    public void run() {
+                        try {
+                            ch.epfl.xblast.client.Main.main(argsClient);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
                     }
-                }
-            };
+                };
+                new Thread(clientMain).start();
+            }
             new Thread(serverMain).start();
-            new Thread(clientMain).start();
+            
             f.dispose();
             
         });
 
+        //3)Panel
+        JPanel serverPanel = new JPanel(new BorderLayout());
+        serverPanel.add(numberText,BorderLayout.NORTH);
+        serverPanel.add(nbPlayers,BorderLayout.WEST);
+        serverPanel.add(checkLaunchClient,BorderLayout.CENTER);
+        serverPanel.add(serverButtton,BorderLayout.EAST);
+        
+        serverPanel.setBorder(BorderFactory.createMatteBorder(10, 10, 10, 10, new Color(0, 125, 255)));
         
         
-        JPanel buttons = new JPanel(new FlowLayout());
-        buttons.add(b1);
-        buttons.add(b2);
-        buttons.add(b3);
-        buttons.add(nombreText);
-        buttons.add(nbPlayers);
+        /*
+         * General Panel
+         */
+        JPanel buttons = new JPanel();
+        buttons.setLayout(new GridLayout(3, 1));
 
+        buttons.add(localPanel);
+        buttons.add(clientPanel);
+        buttons.add(serverPanel);
         
-        p.add(buttons, BorderLayout.PAGE_END);
-        p.setBorder(BorderFactory.createMatteBorder(40, 40, 40, 40, new Color(0, 255, 255)));
-        f.getContentPane().add(p);
+
+        JPanel panel = new JPanel(new BorderLayout());
+        
+        panel.add(title,BorderLayout.NORTH);
+        panel.add(buttons,BorderLayout.SOUTH);
+        panel.setBorder(BorderFactory.createMatteBorder(40, 40, 40, 40, new Color(0, 255, 255)));
+        f.getContentPane().add(panel);
 
         //f.setResizable(false);
         f.pack();
