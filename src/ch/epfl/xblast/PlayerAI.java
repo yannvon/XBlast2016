cea2bd021440856dc4ea03f747f578a83e8127b9
Yann Vonlanthen <yann.vonlanthen@epfl.ch>
2016-05-22 12:04:48 +0200

    Create Class for AI Player

---
+++ src/ch/epfl/xblast/PlayerAI.java
@@ -0,0 +1,22 @@
+package ch.epfl.xblast;
+
+import java.util.List;
+
+import ch.epfl.xblast.server.GameState;
+
+/**
+ * Artificial Intelligence for XBlast 2016.
+ * 
+ * @author Loïc Vandenberghe (257742)
+ * @author Yann Vonlanthen (258857)
+ *
+ */
+public class PlayerAI {
+    private PlayerAI(){}
+    
+    public static List<PlayerAction> nextMove(GameState gameState, PlayerID id){
+        
+        return null;
+    }
+
+}
--- src/ch/epfl/xblast/server/Level.java
+++ src/ch/epfl/xblast/server/Level.java
@@ -3,7 +3,6 @@ package ch.epfl.xblast.server;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
